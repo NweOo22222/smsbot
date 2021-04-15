c58599c977049de397398813221e063756fa2648
etherio <etherio97@users.noreply.github.com>
2021-04-16 00:52:38 +0630

    a

--- dist/routes.js
+++ dist/routes.js
@@ -55,205 +55,127 @@ var DB_1 = __importDefault(require("./app/DB"));
 var socket_1 = require("./socket");
 var config_1 = require("./config");
 var middleware_1 = __importDefault(require("./middleware"));
-var axios_1 = __importDefault(require("axios"));
 var Config_1 = __importDefault(require("./app/Config"));
 var _tasks = {};
 var router = express_1.Router();
 router.get("/call", middleware_1.default, function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
-    var message, phone, keyword, online, devices, data, e_1;
+    var message, phone, keyword;
     return __generator(this, function (_a) {
-        switch (_a.label) {
-            case 0:
-                message = new Message_1.default({
-                    body: decodeURIComponent(String(req.query.message)),
-                    address: req["phone"],
-                });
-                phone = message.phone;
-                keyword = new Keyword_1.default(message.body);
-                online = false, devices = [];
-                _a.label = 1;
-            case 1:
-                _a.trys.push([1, 3, , 4]);
-                return [4, axios_1.default.get("https://api.nweoo.com/device")];
-            case 2:
-                data = (_a.sent()).data;
-                devices = data.filter(function (device) { return device.online && device.operator == phone.operator; });
-                online = Boolean(devices.length);
-                return [3, 4];
-            case 3:
-                e_1 = _a.sent();
-                online = false;
-                devices = [];
-                return [3, 4];
-            case 4:
-                keyword.onAskHelp(function () {
-                    var text = printf_1.default(config_1.ON_HELP, Config_1.default.get("MOBILE_NUMBER"));
-                    phone.incr({
-                        total_action: 1,
-                    });
-                    if (online && devices.length) {
-                        axios_1.default.get("https://api.nweoo.com/device/send?phone=" + phone.number + "&message=" + encodeURIComponent(text));
-                        res.send("");
-                    }
-                    else {
-                        res.send(text);
-                    }
-                    socket_1.io().emit("users:update", phone);
-                });
-                keyword.onAskHeadlines(function () {
-                    var actions = [];
-                    var highlights = Highlight_1.default.get(5, new Date(), phone.highlights).reverse();
-                    var latest = Headline_1.default.latest(5 - highlights.length, phone.headlines).reverse();
-                    var remain = Headline_1.default.latest(0, phone.headlines).length;
-                    var result = __spreadArray(__spreadArray([], highlights), latest);
-                    if (result.length) {
-                        actions.push.apply(actions, result.map(function (_a) {
-                            var title = _a.title, datetime = _a.datetime;
-                            return title +
-                                " " +
-                                datetime.getDate() +
-                                "/" +
-                                Number(datetime.getMonth() + 1);
-                        }));
-                        if (remain > 0 && phone.session.total_action < 1) {
-                            actions.push(printf_1.default(config_1.ON_HEADLINES_NEXT, remain - latest.length));
-                        }
-                        _tasks[message.phone.number] = actions;
-                        phone
-                            .markAsSent(highlights, latest)
-                            .incr({
-                            total_action: 1,
-                        })
-                            .save();
-                        res.end();
-                    }
-                    else {
-                        var text = printf_1.default(config_1.ON_HEADLINES_NULL, Config_1.default.get("MOBILE_NUMBER"));
-                        phone
-                            .markAsSent(highlights, latest)
-                            .incr({
-                            total_action: 1,
-                        })
-                            .save();
-                        if (online && devices.length) {
-                            axios_1.default.get("https://api.nweoo.com/device/send?phone=" + phone.number + "&message=" + encodeURIComponent(text));
-                            res.send("");
-                        }
-                        else {
-                            res.send(text);
-                        }
-                    }
-                    socket_1.io().emit("users:update", phone);
-                });
-                keyword.onAskCount(function () {
-                    var remain = Headline_1.default.latest(0, phone.headlines).length;
-                    var text = printf_1.default(config_1.ON_REMAINING_COUNT, remain);
-                    phone
-                        .incr({
-                        total_action: 1,
-                    })
-                        .save();
-                    if (online && devices.length) {
-                        axios_1.default.get("https://api.nweoo.com/device/send?phone=" + phone.number + "&message=" + encodeURIComponent(text));
-                        res.send("");
-                    }
-                    else {
-                        res.send(text);
-                    }
-                });
-                keyword.onAskReset(function () {
-                    phone
-                        .reset()
-                        .incr({
-                        total_action: 1,
-                    })
-                        .save();
-                    if (online && devices.length) {
-                        axios_1.default.get("https://api.nweoo.com/device/send?phone=" + phone.number + "&message=" + encodeURIComponent(config_1.ON_RESET));
-                        res.send("");
-                    }
-                    else {
-                        res.send(config_1.ON_RESET);
-                    }
-                    socket_1.io().emit("users:update", phone);
-                });
-                keyword.onAskInfo(function () {
-                    res.redirect("/call?phone=" +
-                        phone.number +
-                        "&operator=" +
-                        phone.operator +
-                        "&message=help");
-                    res.end();
-                });
-                keyword.onUnexisted(function () {
-                    var text = printf_1.default(config_1.ON_UNEXISTED, Config_1.default.get("MOBILE_NUMBER"));
-                    phone
-                        .incr({
-                        total_action: 1,
-                    })
-                        .save();
-                    if (online && devices.length) {
-                        axios_1.default.get("https://api.nweoo.com/device/send?phone=" + phone.number + "&operator=" + phone.operator + "&message=" + encodeURIComponent(text));
-                        res.send("");
-                    }
-                    else {
-                        res.send(text);
-                    }
-                    socket_1.io().emit("users:update", phone);
-                });
-                return [2];
-        }
+        message = new Message_1.default({
+            body: decodeURIComponent(String(req.query.message)),
+            address: req["phone"],
+        });
+        phone = message.phone;
+        keyword = new Keyword_1.default(message.body);
+        keyword.onAskHelp(function () {
+            var text = printf_1.default(config_1.ON_HELP, Config_1.default.get("MOBILE_NUMBER"));
+            phone.incr({
+                total_action: 1,
+            });
+            res.send(text);
+            socket_1.io().emit("users:update", phone);
+        });
+        keyword.onAskHeadlines(function () {
+            var actions = [];
+            var highlights = Highlight_1.default.get(5, new Date(), phone.highlights).reverse();
+            var latest = Headline_1.default.latest(5 - highlights.length, phone.headlines).reverse();
+            var remain = Headline_1.default.latest(0, phone.headlines).length;
+            var result = __spreadArray(__spreadArray([], highlights), latest);
+            if (result.length) {
+                actions.push.apply(actions, result.map(function (_a) {
+                    var title = _a.title, datetime = _a.datetime;
+                    return title +
+                        " " +
+                        datetime.getDate() +
+                        "/" +
+                        Number(datetime.getMonth() + 1);
+                }));
+                if (remain > 0 && phone.session.total_action < 1) {
+                    actions.push(printf_1.default(config_1.ON_HEADLINES_NEXT, remain - latest.length));
+                }
+                _tasks[message.phone.number] = actions;
+                phone
+                    .markAsSent(highlights, latest)
+                    .incr({
+                    total_action: 1,
+                })
+                    .save();
+                res.end();
+            }
+            else {
+                var text = printf_1.default(config_1.ON_HEADLINES_NULL, Config_1.default.get("MOBILE_NUMBER"));
+                phone
+                    .markAsSent(highlights, latest)
+                    .incr({
+                    total_action: 1,
+                })
+                    .save();
+                res.send(text);
+            }
+            socket_1.io().emit("users:update", phone);
+        });
+        keyword.onAskCount(function () {
+            var remain = Headline_1.default.latest(0, phone.headlines).length;
+            var text = printf_1.default(config_1.ON_REMAINING_COUNT, remain);
+            phone
+                .incr({
+                total_action: 1,
+            })
+                .save();
+            res.send(text);
+        });
+        keyword.onAskReset(function () {
+            phone
+                .reset()
+                .incr({
+                total_action: 1,
+            })
+                .save();
+            res.send(config_1.ON_RESET);
+            socket_1.io().emit("users:update", phone);
+        });
+        keyword.onAskInfo(function () {
+            res.redirect("/call?phone=" +
+                phone.number +
+                "&operator=" +
+                phone.operator +
+                "&message=help");
+            res.end();
+        });
+        keyword.onUnexisted(function () {
+            var text = printf_1.default(config_1.ON_UNEXISTED, Config_1.default.get("MOBILE_NUMBER"));
+            phone
+                .incr({
+                total_action: 1,
+            })
+                .save();
+            res.send(text);
+            socket_1.io().emit("users:update", phone);
+        });
+        return [2];
     });
 }); });
 router.get("/action", function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
-    var online, devices, text, number, data, e_2, phone;
+    var text, number, phone;
     return __generator(this, function (_a) {
-        switch (_a.label) {
-            case 0:
-                online = false;
-                devices = [];
-                number = req["phone"];
-                _a.label = 1;
-            case 1:
-                _a.trys.push([1, 3, , 4]);
-                return [4, axios_1.default.get("https://api.nweoo.com/device")];
-            case 2:
-                data = (_a.sent()).data;
-                online = true;
-                devices = data.filter(function (device) { return device.online; });
-                return [3, 4];
-            case 3:
-                e_2 = _a.sent();
-                online = false;
-                devices = [];
-                return [3, 4];
-            case 4:
-                if (typeof _tasks[number] !== "object" || !_tasks[number].length) {
-                    return [2, res.status(400).end()];
-                }
-                phone = new Phone_1.default(number);
-                text = _tasks[number].shift();
-                if (!_tasks[number].length) {
-                    _tasks[number] = undefined;
-                    delete _tasks[number];
-                }
-                phone
-                    .incr({
-                    total_action: 0,
-                })
-                    .save();
-                if (online && devices.length) {
-                    __spreadArray([text], _tasks[number]).forEach(function (text) {
-                        return axios_1.default.get("https://api.nweoo.com/device/send?phone=" + phone.number + "&message=" + encodeURIComponent(text));
-                    });
-                    res.send("");
-                }
-                else {
-                    res.send(text);
-                }
-                socket_1.io().emit("users:update", phone);
-                return [2];
+        number = req["phone"];
+        if (typeof _tasks[number] !== "object" || !_tasks[number].length) {
+            return [2, res.status(400).end()];
         }
+        phone = new Phone_1.default(number);
+        text = _tasks[number].shift();
+        if (!_tasks[number].length) {
+            _tasks[number] = undefined;
+            delete _tasks[number];
+        }
+        phone
+            .incr({
+            total_action: 0,
+        })
+            .save();
+        res.send(text);
+        socket_1.io().emit("users:update", phone);
+        return [2];
     });
 }); });
 router.get("/update", function (req, res) {
